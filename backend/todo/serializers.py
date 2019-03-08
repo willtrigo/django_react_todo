7a0d1c7c338fcc40bc46ce7a979538d959b18dfc
Willtrigo <willtrigo@gmail.com>
2019-03-08 17:53:21 -0300

    Add serializers

---
+++ backend/todo/serializers.py
@@ -0,0 +1,13 @@
+"""Serializer settings."""
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    """Serializer Todo."""
+
+    class Meta:
+        """Init serializer todo."""
+
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
